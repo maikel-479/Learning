e104b31021a916c8682e084155459137131c3a5d
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-19 21:27:41 +0100

    style(black-jack): fix indentation to 2 spaces

--- black-jack/script.js
+++ black-jack/script.js
@@ -11,30 +11,30 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
 function startGame() {
-    renderGame()
+  renderGame()
 }
 
 function renderGame() {
-    // 2. Refer to the cards array when rendering out the cards
-    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
-    // cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
-    sumEl.textContent = "Sum: " + sum
-    if (sum <= 20) {
-        message = "Do you want to draw a new card?"
-    } else if (sum === 21) {
-        message = "You've got Blackjack!"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game!"
-        isAlive = false
-    }
-    messageEl.textContent = message
+  // 2. Refer to the cards array when rendering out the cards
+  cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1]
+  // cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+  sumEl.textContent = "Sum: " + sum
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+  } else if (sum === 21) {
+    message = "You've got Blackjack!"
+    hasBlackJack = true
+  } else {
+    message = "You're out of the game!"
+    isAlive = false
+  }
+  messageEl.textContent = message
 }
 
 
 function newCard() {
-    let card = 6
-    sum += card
-    cards.push(card)
-    renderGame()
+  let card = 6
+  sum += card
+  cards.push(card)
+  renderGame()
 }
--- side-quests/loops.js
+++ side-quests/loops.js
@@ -14,3 +14,14 @@ for ( let count = 1;  count < 11;  count += 1 )  {
 for ( let task = 10;  count < 11;  count += 1 )  {
   console.log(task)
 }
+
+for (let i = 0; i < 6; i += 1) {
+  console.log(i)
+}
+
+// Create a for loop that counts from 10 to 100 in steps of 10
+// Use console.log to log out the numbers
+
+for (let count = 10; count <= 100; count += 10) {
+  console.log(count)
+}
