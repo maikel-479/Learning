87070fd45657a2defb0e2b05cab62e7853e944c0
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-19 22:11:32 +0100

    feat(side-quests): add array iteration exercises

--- side-quests/loops.js
+++ side-quests/loops.js
@@ -25,3 +25,20 @@ for (let i = 0; i < 6; i += 1) {
 for (let count = 10; count <= 100; count += 10) {
   console.log(count)
 }
+
+let cards = [7, 3, 9]
+
+// Create a for loop that logs out all the cards in the array
+// Use cards.length to specify how long the loop should run
+
+for (let i = 0; i < cards.length; i += 1) {
+  console.log(cards[i])
+}
+
+let sentence = ["Hello", "my", "name", "is", "Per"]
+let greetingEl = document.getElementById("greeting-el")
+
+// Render the sentence in the greetingEl paragraph using a for loop and .textContent
+for (let i = 0; i < sentence.length; i += 1) {
+  greetingEl.textContent += sentence[i] + " "
+}
