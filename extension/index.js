f78bd461ab3fde34b867c80427c340ac0d70d3b8
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-24 23:41:30 +0100

    feat(extension): add save button click handler
    - Add event listener for save input button
    - Log message on button click

---
+++ extension/index.js
@@ -0,0 +1,6 @@
+// Log out "Button clicked!" when the user clicks the "SAVE INPUT" button
+let saveBtn = document.getElementById("input-btn")
+
+saveBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
