b49fb2390153602453321d2c861894bf8473d59a
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-24 01:38:55 +0100

    feat(challenges): add emoji fighter picker
    - Create random fighter selection from emoji array
    - Add click event listener to pick fighters button
    - Display two random fighters in stage div

---
+++ challenges/phase6.js
@@ -0,0 +1,13 @@
+let fighters = ["🐉", "🐥", "🐊","💩", "🦍", "🐢", "🐩", "🦭", "🦀", "🐝", "🤖", "🐘", "🐸", "🕷","🐆", "🦕", "🦁"]
+
+let stageEl = document.getElementById("stage")
+let fightButton = document.getElementById("fightButton")
+
+fightButton.addEventListener("click", function() {
+  // Challenge:
+  // When the user clicks on the "Pick Fighters" button, pick two random
+  // emoji fighters and display them as i.e. "🦀 vs 🐢" in the "stage" <div>.
+    let fighterOne = Math.floor(Math.random() * fighters.length)
+    let fighterTwo = Math.floor(Math.random() * fighters.length)
+    stageEl.textContent = [fighters[fighterOne] + " vs " + fighters[fighterTwo]]
+})
