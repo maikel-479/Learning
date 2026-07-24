9759a5d3f3e4683ba001f798f63bd4dad1690c2e
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-24 01:37:09 +0100

    feat(challenges): add rock paper scissor random hand
    - Create getRandomHand function using Math.random()
    - Randomly select from rock, paper, scissor array
    - Add console output to demonstrate functionality

---
+++ challenges/phase5.js
@@ -0,0 +1,10 @@
+let hands = ["rock", "paper", "scissor"]
+
+// Create a function that returns a random item from the array
+
+function getRandomHand() {
+  let randomIndex = Math.floor(Math.random() * hands.length);
+  return hands[randomIndex];
+}
+
+console.log(getRandomHand());
