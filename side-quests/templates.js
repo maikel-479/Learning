383dbaf05c41c2b1130a54557aceaa52c4545cca
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-26 15:47:15 +0100

    fix(extension): append leads instead of overwriting
    - Use += instead of = to append all leads to list
    
    feat(side-quests): add sender variable to template exercise
    
    - Add sender variable for personalized email

--- side-quests/templates.js
+++ side-quests/templates.js
@@ -1,9 +1,14 @@
 // template strings/literals
 
 const recipient = "James"
+// Create a new variable, sender, and set its value to your name
+let sender = "Maikel"
 
-// Refactor the email string to use template strings
-// const email = "Hey " + recipient + "! How is it going? Cheers Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+// Use your sender variable instead of "Per"
+const email = `
+  Hey ${recipient}!
+  How is it going?
+  Cheers ${sender}
+`
 
 console.log(email)
