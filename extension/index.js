8c2e95b1feaa9c542c6f334cb6c942cd8837f0c8
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-26 02:33:45 +0100

    feat(extension): push input value to leads array
    - Replace hardcoded string with inputEl.value

--- extension/index.js
+++ extension/index.js
@@ -2,9 +2,10 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
-// Push the value "www.awesomelead.com" to myArray when the input button is clicked
-
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked!")
-    myLeads.push("www.awesomelead.com")
+    // Push the value from the inputEl into the myLeads array
+    // instead of the hard-coded "www.awesomeleads.com" value
+    // Google -> "get value from input field javascript"
+    myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
