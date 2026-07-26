87c7e7f199812d1856d1f4d789c5ed090afd0263
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-26 13:40:52 +0100

    refactor(side-quests): extract renderLeads function
    - Wrap lead rendering in reusable renderLeads function
    - Clear array to start fresh

--- side-quests/innerhtml.js
+++ side-quests/innerhtml.js
@@ -1,14 +1,19 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    // 2. Call the renderLeads() function
+    renderLeads()
 })
 
-// Replace .textContent with .innerHTML and use <li> tags
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+// 1. Wrap the code below in a renderLeads() function
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+  }
+  ulEl.innerHTML = listItems
 }
