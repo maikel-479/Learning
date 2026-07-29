72ce142316c297b5410dbb119e60895cbac84514
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-30 00:38:28 +0100

    feat(side-quests): add truely-falsely boolean examples
    - Add examples of Boolean() conversion
    - Show truthy and falsy values in JavaScript

---
+++ side-quests/truely-falsely.js
@@ -0,0 +1,9 @@
+console.log(  Boolean("")   ) //false
+console.log(  Boolean(" ")  ) //true
+console.log(  Boolean(0)    ) //false
+console.log(  Boolean(-1)   ) //true
+console.log(  Boolean("0")  ) //true
+console.log(  Boolean(100)  ) //true
+console.log(  Boolean(null) ) //false
+console.log(  Boolean([0])  ) //true
+console.log(  Boolean(-0)   ) //false
