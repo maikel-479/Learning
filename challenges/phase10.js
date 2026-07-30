a5a39a0228d8809dfad73d9601d4b3bf618c3390
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-31 00:13:40 +0100

    feat(challenges): add phase10.js

---
+++ challenges/phase10.js
@@ -0,0 +1,6 @@
+// Save a value to localStorage
+// Delete your code and refresh the page
+// Fetch your value from localStorage and log it out
+localStorage.setItem("myCat", "Tom")
+let cat = localStorage.getItem("myCat")
+console.log(cat)
