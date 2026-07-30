74877c99242df16ee88b0e14402c1c336c17397a
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-30 03:32:25 +0100

    feat(side-quests): add function with parameters exercise

---
+++ side-quests/no_func_params.js
@@ -0,0 +1,8 @@
+// Create a function, add(), that adds two numbers together and returns the sum
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum
+}
+
+console.log( add(3, 4)   ) // should log 7
+console.log( add(9, 102) ) // should log 111
