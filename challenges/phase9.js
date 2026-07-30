631e43dc54cfc78a09251a8a039e342511eb33de
maikel-479 <maikel-479@users.noreply.github.com>
2026-07-31 00:13:36 +0100

    feat(challenges): add phase9.js

---
+++ challenges/phase9.js
@@ -0,0 +1,12 @@
+const myCourses = ["Learn CSS Animations", "UI Design Fundamentals", "Intro to Clean Code"]
+
+// Create a function that takes a single parameter, an array,
+// and logs all the items of the array to the console.
+// Call the function while passing in myCourses as an argument
+function logCourses(courses) {
+    for (let i = 0; i < courses.length; i++) {
+        console.log(courses[i]);
+    }
+}
+
+logCourses(myCourses);
